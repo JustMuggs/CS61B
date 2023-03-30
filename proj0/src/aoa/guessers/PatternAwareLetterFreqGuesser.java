172045f04679f19bcaadaca3826c2ffce91026a0
JustMuggs <mottodj@gmail.com>
2023-03-30 15:06:39 +0300

    proj0 PatternAwareGuesser

--- proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
@@ -1,8 +1,11 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class PatternAwareLetterFreqGuesser implements Guesser {
     private final List<String> words;
@@ -15,8 +18,66 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
     /** Returns the most common letter in the set of valid words based on the current
      *  PATTERN. */
     public char getGuess(String pattern, List<Character> guesses) {
-        // TODO: Fill in this method.
+        Map<Character, Integer> map = this.getFreqMapThatMatchesPattern(pattern);
+        for (Character c : guesses) {
+            if (map.containsKey(c)) {
+                map.remove(c);
+            }
+        }
+        int max = 0;
+        for (Integer i : map.values()) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        for (Character key : map.keySet()) {
+            if (map.get(key) == max) {
+                return key;
+            }
+        }
         return '?';
+    }
+
+    public List<String> keepOnlyWordsThatMatchPattern(String pattern) {
+        List<String> matchingWords = new ArrayList<>();
+        for (String word : words) {
+            if (word.length() != pattern.length()) {
+                continue;
+            }
+            boolean same = true;
+            for (int i = 0; i < word.length(); i++) {
+                if (pattern.charAt(i) != '-' && pattern.charAt(i) != word.charAt(i)) {
+                    same = false;
+                    break;
+                }
+            }
+            if (same) {
+                matchingWords.add(word);
+            }
+        }
+        return matchingWords;
+    }
+
+    public Map<Character, Integer> getFreqMapThatMatchesPattern(String pattern) {
+        Map<Character, Integer> map = new TreeMap<>();
+        List<String> matchingWords = this.keepOnlyWordsThatMatchPattern(pattern);
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < 26; i++) {
+            char letter = alphabet.charAt(i);
+            int count = 0;
+            for (String word : matchingWords) {
+                for (int j = 0; j < word.length(); j++) {
+                    if (word.charAt(j) == letter) {
+                        count++;
+                    }
+                }
+            }
+            if (count > 0) {
+                map.put(letter, count);
+            }
+        }
+        return map;
+
     }
 
     public static void main(String[] args) {
